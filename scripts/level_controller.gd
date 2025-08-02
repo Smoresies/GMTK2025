f1481f4fc130a1c7f20bba2dc2db08dc9ee7c49a
Smoresies <gavin.britton@gmail.com>
2025-08-02 14:15:35 -0700

    Made level 2 and 3, added button back to main menu

--- scripts/level_controller.gd
+++ scripts/level_controller.gd
@@ -40,3 +40,7 @@ func _trigger_end():
 
 func _on_next_level_pressed() -> void:
 	get_tree().change_scene_to_packed(nextLevel)
+
+
+func _on_main_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/levels/main_menu.tscn")
