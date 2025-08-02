1e15105791b2277a335838df4b38fd70d618365d
Smoresies <gavin.britton@gmail.com>
2025-08-02 16:36:28 -0700

    Cleaned up boards to look nicer. Completed level 6

--- scripts/board_space.gd
+++ scripts/board_space.gd
@@ -2,9 +2,14 @@ class_name BoardSpace
 extends StaticBody2D
 
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
+@export var texture : CompressedTexture2D = null
 var is_empty = true
 
 func _ready():
+	if texture:
+		sprite_2d.texture = texture
 	pass
 	# modulate = Color(Color.MEDIUM_PURPLE, 0.7)
 
