328ceb67cc70d357e047b9f1be11bdfaf1465288
Smoresies <gavin.britton@gmail.com>
2025-08-01 23:22:16 -0700

    Added side objective art and "animation"

--- scenes/side_objective.gd
+++ scenes/side_objective.gd
@@ -1,5 +1,8 @@
 extends Area2D
 
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
 func _on_area_shape_entered(_area_rid: RID, _area: Area2D, _area_shape_index: int, _local_shape_index: int) -> void:
 	global.connections += 1
 	global.side_objectives += 1
+	sprite_2d.frame = 1
