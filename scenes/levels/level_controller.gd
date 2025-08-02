0f917ec475a2e85b50de3974097043ece3f64eb0
Smoresies <gavin.britton@gmail.com>
2025-08-01 23:34:00 -0700

    Started bulb stuff

--- scenes/levels/level_controller.gd
+++ scenes/levels/level_controller.gd
@@ -12,7 +12,15 @@ func _ready():
 	global.can_interact = true
 
 func _trigger_end():
+	### Open Menu & Stop Game Interaction
 	level_complete.visible = true
 	global.can_interact = false
+	
+	### Stars Counting
+	# See how many bulbs gotten vs total bulbs. 
+	# Subtract from 3, for number of bulbs at level end screen given
+	var bulbs = 3 - (number_of_side_objectives - global.side_objectives)
+	
+	### Level Transition
 	# await get_tree().create_timer(1).timeout
 	# get_tree().change_scene_to_packed(nextLevel)
--- scripts/global.gd
+++ scripts/global.gd
@@ -9,5 +9,3 @@ var expected_connections : int = 0
 var connections : int = 0
 
 var side_objectives : int = 0
-
-var number_of_side_objectives
