0c679c3e33148fd5c07416468968b6ece93557fd
Smoresies <gavin.britton@gmail.com>
2025-08-03 02:53:22 -0700

    Slight level tweaks

--- scenes/levels/subtitle.gd
+++ scenes/levels/subtitle.gd
@@ -1,8 +1,15 @@
 extends Label
 
+var tween
+
 func _ready():
 	var init_scale := scale
-	var tween = get_tree().create_tween().set_loops()
+	tween = get_tree().create_tween().set_loops()
 	tween.tween_property(self, "scale", scale * 0.9, 1).set_ease(Tween.EASE_IN_OUT)
-	tween.tween_property(self, "scale", init_scale, 1).set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property(self, "scale", init_scale, 1).set_ease(Tween.EASE_IN_OUT).set_delay(0.1)
 	pass
+
+func _notification(what: int) -> void:
+	match what:
+		NOTIFICATION_PREDELETE:
+			tween.stop()
