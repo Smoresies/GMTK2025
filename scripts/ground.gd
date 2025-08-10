2b529f1e5a646ed20f64c499f457034fbfbad06d
Smoresies <gavin.britton@gmail.com>
2025-08-09 19:15:03 -0700

    Fixed electricity particle bug with grounds and bulbs

--- scripts/ground.gd
+++ scripts/ground.gd
@@ -1,13 +1,16 @@
 extends Area2D
 
-@onready var electricity: Line2D = $Electricity
+@onready var electricity: Line2D = $"../Electricity"
+@onready var collision_shape_2d: CollisionShape2D = $"../Area2D/CollisionShape2D"
 
 func _ready() -> void:
 	global.level_not_complete.connect(_reset)
 
 func _on_area_shape_entered(_area_rid: RID, _area: Area2D, _area_shape_index: int, _local_shape_index: int) -> void:
-	global.connections += 1
+	# global.connections += 1
 	electricity.visible = true
+	collision_shape_2d.set_deferred("disabled", false)
 
 func _reset():
 	electricity.visible = false
+	collision_shape_2d.set_deferred("disabled", true)
--- scripts/side_objective.gd
+++ scripts/side_objective.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var electricity: Line2D = $Electricity
+@onready var collision_shape_2d: CollisionShape2D = $"../Area2D/CollisionShape2D"
 
 func _ready() -> void:
 	global.level_not_complete.connect(_reset)
@@ -10,9 +11,11 @@ func _ready() -> void:
 func _reset():
 	electricity.visible = false
 	sprite_2d.frame = 0
+	collision_shape_2d.set_deferred("disabled", true)
 
 func _on_area_shape_entered(_area_rid: RID, _area: Area2D, _area_shape_index: int, _local_shape_index: int) -> void:
-	global.connections += 1
+	# global.connections += 1
 	global.side_objectives += 1
 	sprite_2d.frame = 1
 	electricity.visible = true
+	collision_shape_2d.set_deferred("disabled", false)
